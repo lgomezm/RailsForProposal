9d2524ad1f43d5766f888704bf4fe6b292897c9c
Enrique Arango Lyons <karlysw@gmail.com>
2011-05-18 14:23:19 -0500

    form and table

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,4 +4,22 @@ class RequestsController < ApplicationController
     @requests = Request.all
   end
   
+  def show
+    @request = Request.find(params[:id])
+  end
+  
+  def new
+    @request = Request.new
+  end
+  
+  def create
+    @request = Request.new(params[:request])
+    if @request.save
+      flash[:notice] = "RFP creado"
+      redirect_to requests_path
+    else
+      render 'new'
+    end
+  end
+  
 end
